c767c3753162941a2efe2db4b3388f1cbe7fd4e3
Paweł Grabowski <gprabowski@outlook.com>
2023-06-12 12:21:26 +0200

    Update example config

--- includes/config.hpp
+++ includes/config.hpp
@@ -5,7 +5,7 @@ namespace config {
 
 using CT = thrust::complex<double>;
 
-constexpr int radix = 8;
-constexpr int N = 4096;
+constexpr int radix = 16;
+constexpr int N = radix * radix;
 
 } // namespace config
