2c9cb44fec628b4ef77de1bb730bf1298cbae9af
Pawel Grabowski <gprabowski@outlook.com>
2023-06-26 04:41:07 +0200

    Overlap stores with compute and twiddle in constructor

--- includes/tensor_fft_64.cuh
+++ includes/tensor_fft_64.cuh
@@ -18,6 +18,12 @@ struct tensor_fft_64 {
   static constexpr auto ffts_per_unit = FPU;
   static constexpr auto max_threads_per_block = units_per_block * threads;
 
+  mma_fp64_884_indexes indexing;
+  const CT twiddle1 = pow_theta<64>(indexing.crow * indexing.ccol);
+  const CT twiddle2 = pow_theta<64>(indexing.crow * (indexing.ccol + 1));
+  const CT a1 = pow_theta<8>(indexing.arow * indexing.acol);
+  const CT a2 = pow_theta<8>(indexing.arow * (indexing.acol + 4));
+
   static_assert(Size == 64, "SIZE MUST BE 64");
 
   template <int N> inline __device__ CT pow_theta(int p) const {
@@ -44,7 +50,6 @@ struct tensor_fft_64 {
         (sh_d + Size * ffts_per_unit * (block.thread_rank() / threads));
 
     // 0. Prepare mma and transpose indices
-    mma_fp64_884_indexes indexing;
 
     // 1. Pre-load b for 1st iter
     // in here we tranpose the matrix (as its naturally
@@ -52,18 +57,9 @@ struct tensor_fft_64 {
     local_b[0] = local_data[indexing.brow * 8 + indexing.bcol];
     local_b[1] = local_data[(indexing.brow + 4) * 8 + indexing.bcol];
 
-    // 2. Fill A "Matrix" with twiddles
-    const CT a1 = pow_theta<8>(indexing.arow * indexing.acol);
-    const CT a2 = pow_theta<8>(indexing.arow * (indexing.acol + 4));
-
-    // 3. Pre-compute twiddles
-    const CT twiddle1 = pow_theta<64>(indexing.crow * indexing.ccol);
-    const CT twiddle2 = pow_theta<64>(indexing.crow * (indexing.ccol + 1));
-
 #pragma unroll
     for (int i = 0; i < ffts_per_unit; ++i) {
-
-      // 4. Pre-load B elements for next iteration
+      // 2. Pre-load B elements for next iteration
       if (i < ffts_per_unit - 1) {
         local_b[2 * (i + 1)] =
             local_data[(i + 1) * Size + indexing.brow * 8 + indexing.bcol];
@@ -72,15 +68,12 @@ struct tensor_fft_64 {
                        indexing.bcol];
       }
 
+      // 3. Compute FFT on 64 elements
       fft_kernels::c64_fft64<CT>(a1, a2, local_b[2 * i], local_b[2 * i + 1],
                                  twiddle1, twiddle2, indexing.transpose_lane_b1,
                                  indexing.transpose_lane_b2);
-    }
 
-#pragma unroll
-    for (int i = 0; i < ffts_per_unit; ++i) {
-      // 5. Save results
-      // perform digit reversal (that's why indexing is reversed)
+      // 4. Overlap store with next iteration
       local_data[i * Size + indexing.crow * 8 + indexing.ccol] = local_b[2 * i];
       local_data[i * Size + indexing.crow * 8 + (indexing.ccol + 1)] =
           local_b[2 * i + 1];
